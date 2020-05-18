100797b641c32aff41f020108917822c3be83819
ChiyosBigDragon <chiyotairyuhidemasa@gmail.com>
2020-05-19 04:35:08 +0900

    create RicochetRobots

--- src/index.tsx
+++ src/index.tsx
@@ -1,135 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Square from "./component/RicochetRobots/Square";
 import './index.css';
 
-type SquareType = string | null;
-interface SquareProps {
-	value: SquareType;
-	onClick: () => void;
-}
-
-function Square(props: SquareProps) {
-	return (
-		<button className="square" onClick={props.onClick}>
-			{props.value}
-		</button>
-	);
-}
-
 interface BoardProps {
-	squares: SquareType[];
-	onClick: (i: number) => void;
+	size: number;
+	wall: boolean[][][];
 }
 
 class Board extends React.Component<BoardProps> {
-	renderSquare(i: number) {
-		return (<Square value={this.props.squares[i]} onClick={() => this.props.onClick(i)}/>);
+	renderSquare(wall: boolean[]) {
+		return (<Square wall={wall} robotProps={{id: 1, color: "#ff1744"}} />);
 	}
 	render() {
+		const wall = this.props.wall;
+		const size = this.props.size;
+		const squares = Array(size);
+		for(let y = 0; y < size; ++y) {
+			const row = Array(size);
+			for(let x = 0; x < size; ++x) {
+				row[x] = this.renderSquare(wall[y][x]);
+			}
+			squares[y] = <div className="board-row">{row}</div>
+		}
 		return (
 			<div>
-				<div className="board-row">
-					{this.renderSquare(0)}
-					{this.renderSquare(1)}
-					{this.renderSquare(2)}
-				</div>
-				<div className="board-row">
-					{this.renderSquare(3)}
-					{this.renderSquare(4)}
-					{this.renderSquare(5)}
-				</div>
-				<div className="board-row">
-					{this.renderSquare(6)}
-					{this.renderSquare(7)}
-					{this.renderSquare(8)}
-				</div>
+				{squares}
 			</div>
 		);
 	}
 }
 
-function calculateWinner(squares: SquareType[]) {
-	const lines = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		[0, 4, 8],
-		[2, 4, 6],
-	];
-	for(let i = 0; i < lines.length; ++i) {
-		const [a, b, c] = lines[i];
-		if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-			return squares[a];
-		}
-	}
-	return null;
+
+interface RicochetRobotsState {
+	// step: number;
+	size: number;
+	wall: boolean[][][];
+	// robots: {x: number, y: number, select: boolean}[];
+	// goal: boolean[][];
+	// history: {squares: SquareType[]}[];
+	// stepNumber: number;
+	// xIsNext: boolean;
 }
 
-interface GameState {
-	history: {squares: SquareType[]}[];
-	stepNumber: number;
-	xIsNext: boolean;
-}
-
-class Game extends React.Component<any, GameState> {
-	constructor(props: GameState) {
+class RicochetRobots extends React.Component<any, RicochetRobotsState> {
+	constructor(props: RicochetRobotsState) {
 		super(props);
 		this.state = {
-			history: [{squares: Array<SquareType>(9).fill(null)}],
-			stepNumber: 0,
-			xIsNext: true
+			size: 16,
+			wall: Array(16).fill(Array(16).fill(Array(4).fill(true)))
+			// wall = [...Array(3)].map(() => Array(3).fill(0));
+			// history: [{squares: Array<SquareType>(9).fill(null)}],
+			// stepNumber: 0,
+			// xIsNext: true
 		};
 	}
-	handleClick(i: number) {
-		const history = this.state.history.slice(0, this.state.stepNumber + 1);
-		const current = history[history.length - 1];
-		const squares = current.squares.slice();
-		if(calculateWinner(squares) || squares[i]) {
-			return;
-		}
-		squares[i] = this.state.xIsNext ? 'X' : 'O';
-		this.setState({
-			history: history.concat([{squares: squares}]),
-			stepNumber: history.length,
-			xIsNext: !this.state.xIsNext
-		});
-	}
-	jumpTo(step: number) {
-		this.setState({
-			stepNumber: step,
-			xIsNext: (step % 2) === 0
-		});
-	}
 	render() {
-		const history = this.state.history;
-		const current = history[this.state.stepNumber];
-		const winner = calculateWinner(current.squares);
-		const moves = history.map((step, move) => {
-			const desc = move ? `Go to move #${move}` : 'Go to game start';
-			return (
-				<li key={move}>
-					<button onClick={() => this.jumpTo(move)}>{desc}</button>
-				</li>
-			);
-		});
-		let status: string;
-		if(winner) {
-			status = `Winner: ${winner}`;
-		} else {
-			status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
-		}
+		const wall = this.state.wall;
+		const size = this.state.size;
 		return (
 			<div className="game">
 				<div className="game-board">
-					<Board squares={current.squares} onClick={(i) => this.handleClick(i)}/>
+					<Board size={size} wall={wall} />
 				</div>
-				<div className="game-info">
+				{/* <div className="game-info">
 					<div>{status}</div>
 					<ol>{moves}</ol>
-				</div>
+				</div> */}
 			</div>
 		);
 	}
@@ -138,6 +75,6 @@ class Game extends React.Component<any, GameState> {
 // ========================================
 
 ReactDOM.render(
-	<Game />,
+	<RicochetRobots />,
 	document.getElementById('root')
 );
